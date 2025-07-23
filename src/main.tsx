7089ac9df4528fff36ff554fe1f97a3503dcb4f0
rory-hayes <54278439+rory-hayes@users.noreply.github.com>
2025-07-23 20:28:44 +0100

    Add landing page and basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import LandingPage from './pages/LandingPage.tsx';
 import './index.css';
+
+const isApp = window.location.pathname.startsWith('/app');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isApp ? <App /> : <LandingPage />}
   </StrictMode>
 );
